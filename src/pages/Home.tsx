802516b08c5eb0c3f16260133bd26c7c7d94c458
Isaac Shea <isaac_shea@icloud.com>
2024-10-26 21:10:14 +1000

    Added products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
-import { Flex, FlexCenter, IconButton, Image, Text, Title } from "@valence-ui/core";
+import { Flex, FlexCenter, Grid, IconButton, Image, Space, Text, Title } from "@valence-ui/core";
 import { useTranslation } from "react-i18next";
+import { IconBrandBluesky, IconBrandGithub, IconBrandInstagram, IconBrandThreads, IconMail } from "@tabler/icons-react";
 
 import avatarImage from "../assets/images/avatar.png";
-import { IconBrandBluesky, IconBrandGithub, IconBrandInstagram, IconBrandThreads, IconMail } from "@tabler/icons-react";
+import threaditorImage from "../assets/images/products/threaditor.png";
+import valenceImage from "../assets/images/products/valence.png";
+import filedropImage from "../assets/images/products/filedrop.png";
+import realmixImage from "../assets/images/products/realmix.png";
+import ProductCard from "../components/ProductCard";
 
 export default function HomePage() {
   const { t } = useTranslation("home");
@@ -13,10 +18,12 @@ export default function HomePage() {
         innerProps={{
           align: "center",
           width: "100%",
-          style: { maxWidth: 450 }
+          style: { maxWidth: 500 }
         }}
-        padding={40}
-        style={{ minHeight: "100vh" }}
+        style={{
+          minHeight: "100vh",
+          padding: "40px 15px"
+        }}
       >
         <Image
           src={avatarImage}
@@ -32,6 +39,7 @@ export default function HomePage() {
           {t("hero.subtitle")}
         </Text>
 
+        {/* Socials buttons */}
         <Flex gap={5}>
           <IconButton
             variant="paper"
@@ -45,7 +53,7 @@ export default function HomePage() {
             variant="paper" color="blue"
             tooltip="@isaacshea.com"
             component="a" href="https://bsky.app/profile/isaacshea.com" target="_blank"
-            >
+          >
             <IconBrandBluesky />
           </IconButton>
 
@@ -53,26 +61,59 @@ export default function HomePage() {
             variant="paper" color="violet"
             tooltip="@zaccomode"
             component="a" href="https://github.com/zaccomode" target="_blank"
-            >
+          >
             <IconBrandGithub />
           </IconButton>
 
           <IconButton
-            variant="paper" color="orange"
+            variant="paper" color="pink"
             tooltip="@zaccomode"
             component="a" href="https://www.instagram.com/zaccomode/" target="_blank"
-            >
+          >
             <IconBrandInstagram />
           </IconButton>
 
           <IconButton
-            variant="paper" color="green"
+            variant="paper" color="orange"
             tooltip="isaac_shea@icloud.com"
             component="a" href="mailto:isaac_shea@icloud.com" target="_blank"
           >
             <IconMail />
           </IconButton>
         </Flex>
+
+        <Space height={10} />
+
+        {/* Products */}
+        <Grid columns={2} width="100%" gap={5}>
+          <ProductCard
+            image={threaditorImage}
+            imageAlt={t("projects.threaditor.imageAlt")}
+            title={t("projects.threaditor.name")}
+            description={t("projects.threaditor.description")}
+          />
+
+          <ProductCard
+            image={valenceImage}
+            imageAlt={t("projects.valence.imageAlt")}
+            title={t("projects.valence.name")}
+            description={t("projects.valence.description")}
+          />
+
+          <ProductCard
+            image={filedropImage}
+            imageAlt={t("projects.filedrop.imageAlt")}
+            title={t("projects.filedrop.name")}
+            description={t("projects.filedrop.description")}
+          />
+
+          <ProductCard
+            image={realmixImage}
+            imageAlt={t("projects.realmix.imageAlt")}
+            title={t("projects.realmix.name")}
+            description={t("projects.realmix.description")}
+          />
+        </Grid>
       </FlexCenter>
     </>
   )
